8924c220f0f7416a6b884afb19e0f84411e240f2
jinzaizhichi <jinzaizhichi9888@gmail.com>
2022-03-29 18:30:57 +0800

    set settings.lua

---
+++ lua/basic/settings.lua
@@ -0,0 +1,23 @@
+--设定各种文本的字符编码
+vim.o.encoding = "utf-8"
+--设定无操作时，交换文件刷写磁盘的等待毫秒数(默认为4000)
+vim.o.updatetime = 100
+--设定等待按键时长毫秒数
+vim.o.timeoutlen = 500
+--是否在屏幕最后一行显示命令
+vim.o.showcmd = true
+--是否允许缓冲区未保存就切换
+vim.o.hidden = true
+--是否开启xterm兼容的终端24位色彩支持
+vim.o.termguicolors = true
+--是否高亮当前文本行
+vim.o.cursorline = true
+--是否开启语法高亮
+vim.o.syntax = "enable"
+--是否显示绝对行号
+vim.o.number = true
+--是否显示相对行号
+vim.o.relativenumber = true
+--设定
+
+
