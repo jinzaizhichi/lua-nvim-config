c2b22a23ee90249c1d66cad33de85e3aa0a64acd
jinzaizhichi <jinzaizhichi9888@gmail.com>
2022-03-30 11:29:38 +0800

    update keybinds.lua

--- lua/basic/keybinds.lua
+++ lua/basic/keybinds.lua
@@ -29,3 +29,16 @@ vim.keybinds.gmap("v","L","$",vim.keybinds.opts)
 --C-u和 C-d 调整为上下滑动10行而不是半页
 vim.keybinds.gmap("n","<C-u>","10k",vim.keybinds.opts)
 vim.keybinds.gmap("n","<C-d>","10j",vim.keybinds.opts)
+
+
+--插入模式下的上下左右移动
+vim.keybinds.gmap("i","<A-k>","<up>",vim.keybinds.opts)
+vim.keybinds.gmap("i","<A-j>","<down>",vim.keybinds.opts)
+vim.keybinds.gmap("i","<A-h>","<left>",vim.keybinds.opts)
+vim.keybinds.gmap("i","<A-l>","<right>",vim.keybinds.opts)
+
+
+--修改分屏大小
+vim.keybinds.gmap("n","<C-up>","<cmd>res +1<CR>",vim.keybinds.opts)
+
+
