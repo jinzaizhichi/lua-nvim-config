852332d2f19f08a47bd581ea67b40239755dbddb
jinzaizhichi <jinzaizhichi9888@gmail.com>
2022-05-03 21:25:53 +0800

    add

--- lua/lsp/pyright.lua
+++ lua/lsp/pyright.lua
@@ -1,20 +1,23 @@
 
 
-return {
-    root_dir = function()
-        return vim.fn.getcwd()
-    end,
-    -- 禁用 Pyright 的诊断信息（使用 pylint）
-    handlers = {
-        ---@diagnostic disable-next-line: unused-vararg
-        ["textDocument/publishDiagnostics"] = function(...)
-        end
-    },
-    settings = {
-        python = {
-            analysis = {
-                typeCheckingMode = "off"
-            }
-        }
-    }
-}
+--return {
+--    root_dir = function()
+--        return vim.fn.getcwd()
+--    end,
+--    -- 禁用 Pyright 的诊断信息（使用 pylint）
+--    handlers = {
+--        ---@diagnostic disable-next-line: unused-vararg
+--        ["textDocument/publishDiagnostics"] = function(...)
+--        end
+--    },
+--    settings = {
+--        python = {
+--            analysis = {
+--                typeCheckingMode = "off"
+--            }
+--        }
+--    }
+--}
+
+
+--
